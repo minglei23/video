31886f31c99fc758b65910d30ac78dab1b3c1999
minglei.23 <minglei.23@hotmail.com>
2023-11-29 12:07:19 -0400

    improve

--- src/Login.js
+++ src/Login.js
@@ -1,86 +1,84 @@
 import React, { useState, useContext } from 'react';
-import { Button, Typography, Container, Box, TextField } from '@mui/material';
+import { Button, Typography, Container, Box, TextField, Grid, Snackbar } from '@mui/material';
 import { login, register } from './service';
 import { UserContext } from './index.js';
 
 const Login = () => {
   const [isLoginView, setIsLoginView] = useState(true);
-  const { user, setUser } = useContext(UserContext);
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const { setUser } = useContext(UserContext);
+  const [error, setError] = useState('');
 
-  // user has ID (int), Email (string), Activated (bool), VIP (bool)
-
-  const handleSubmit = async (event) => {
+  const handleSubmit = async (event, email, password) => {
     event.preventDefault();
-    if (isLoginView) {
-      try {
-        const userData = await login(email, password);
-        setUser(userData);
-      } catch (error) {
-        console.error('login failed:', error);
-      }
-    } else {
-      try {
-        const userData = await register(email, password);
-        setUser(userData);
-      } catch (error) {
-        console.error('register failed:', error);
-      }
+    try {
+      const userData = isLoginView ? await login(email, password) : await register(email, password);
+      setUser(userData);
+    } catch (error) {
+      setError(error.message);
+      console.error(isLoginView ? 'login failed:' : 'register failed:', error);
     }
   };
 
   const toggleView = () => {
     setIsLoginView(!isLoginView);
+    setError('');
   };
 
-  const renderLoginForm = () => {
+  const AuthForm = ({ isLogin }) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
     return (
-      <Container>
-        <Box textAlign="center" marginTop={4}>
-          <Typography variant="h5">{isLoginView ? 'Login' : 'Sign Up'}</Typography>
-          <form onSubmit={handleSubmit}>
-            <TextField
-              label="Email"
-              type="email"
-              fullWidth
-              margin="normal"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <TextField
-              label="Password"
-              type="password"
-              fullWidth
-              margin="normal"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <Button
-              type="submit"
-              variant="contained"
-              color="primary"
-              style={{ margin: '20px' }}
-            >
-              {isLoginView ? 'Login' : 'Sign Up'}
-            </Button>
-            <Button
-              variant="outlined"
-              color="primary"
-              onClick={toggleView}
-            >
-              {isLoginView ? 'Go to Sign Up' : 'Go to Login'}
-            </Button>
-          </form>
-        </Box>
-      </Container>
+      <form onSubmit={(e) => handleSubmit(e, email, password)}>
+        <TextField
+          label="Email"
+          type="email"
+          fullWidth
+          margin="normal"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <TextField
+          label="Password"
+          type="password"
+          fullWidth
+          margin="normal"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Button
+          type="submit"
+          variant="contained"
+          color="primary"
+          fullWidth
+          style={{ marginTop: 20 }}
+        >
+          {isLogin ? 'Login' : 'Sign Up'}
+        </Button>
+      </form>
     );
   };
 
   return (
-    <div>
-      {renderLoginForm()}
-    </div>
+    <Container maxWidth="sm">
+      <Box textAlign="center" marginTop={8}>
+        <Typography variant="h4">{isLoginView ? 'Login' : 'Sign Up'}</Typography>
+        <AuthForm isLogin={isLoginView} />
+        <Grid container justifyContent="center" style={{ marginTop: 20 }}>
+          <Button variant="outlined" color="primary" onClick={toggleView}>
+            {isLoginView ? 'Go to Sign Up' : 'Go to Login'}
+          </Button>
+        </Grid>
+        {error && (
+          <Snackbar
+            open={Boolean(error)}
+            autoHideDuration={6000}
+            onClose={() => setError('')}
+            message={error}
+          />
+        )}
+      </Box>
+    </Container>
   );
 };
 
--- src/Profile.js
+++ src/Profile.js
@@ -1,19 +1,65 @@
-import React, { useContext } from 'react';
-import { Typography, Container, Box } from '@mui/material';
+import React, { useState, useContext } from 'react';
+import { Typography, Container, Box, Button, Modal } from '@mui/material';
 import { UserContext } from './index.js';
 import Login from './Login';
 
 const Profile = () => {
-  const { user, setUser } = useContext(UserContext);
+  const { user } = useContext(UserContext);
+  const [openVerifyModal, setOpenVerifyModal] = useState(false);
 
-  // user has ID (int), Email (string), Activated (bool), VIP (bool)
+  const handleOpenVerifyModal = () => {
+    setOpenVerifyModal(true);
+  };
+
+  const handleCloseVerifyModal = () => {
+    setOpenVerifyModal(false);
+  };
+
+  const renderVerifyEmailModal = () => {
+    return (
+      <Modal
+        open={openVerifyModal}
+        onClose={handleCloseVerifyModal}
+        aria-labelledby="verify-email-modal-title"
+      >
+        <Box sx={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', width: 400, bgcolor: 'background.paper', boxShadow: 24, p: 4, }}>
+          <Typography id="verify-email-modal-title" variant="h6" component="h2">
+            Please Verify Your Email
+          </Typography>
+          <Button onClick={handleCloseVerifyModal} style={{ marginTop: 20 }}>
+            Verify Email
+          </Button>
+          <Button onClick={handleCloseVerifyModal} style={{ marginTop: 20 }}>
+            Close
+          </Button>
+        </Box>
+      </Modal>
+    );
+  };
 
   const renderUserProfile = () => {
     return (
       <Container>
         <Box textAlign="center" marginTop={4}>
           <Typography variant="h5">Welcome, {user.Email}</Typography>
+          {!user.Activated && (
+            <Button variant="contained" onClick={handleOpenVerifyModal} style={{ margin: 2 }}>
+              Verify Email
+            </Button>
+          )}
+          {!user.VIP && (
+            <Button variant="contained" onClick={handleOpenVerifyModal} style={{ margin: 2 }}>
+              Become VIP
+            </Button>
+          )}
+          <Button variant="contained" onClick={handleOpenVerifyModal} style={{ margin: 2 }}>
+            History
+          </Button>
+          <Button variant="contained" onClick={handleOpenVerifyModal} style={{ margin: 2 }}>
+            Favorites
+          </Button>
         </Box>
+        {renderVerifyEmailModal()}
       </Container>
     );
   };
