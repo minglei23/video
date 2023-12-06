a659ef48b51efc7d9a8b6279c171caed3b2141c9
minglei.23 <minglei.23@hotmail.com>
2023-12-05 23:28:09 -0400

    POINTS

--- src/cache.js
+++ src/cache.js
@@ -45,3 +45,26 @@ export const GetToken = () => {
   return item.value;
 };
 
+export const SetCache = (key, value) => {
+  const now = new Date();
+  const item = {
+      value: value,
+      expiry: now.getTime() + 60 * 60 * 1000,
+  };
+  localStorage.setItem(key, JSON.stringify(item));
+};
+
+export const GetCache = (key) => {
+  const itemStr = localStorage.getItem(key);
+  if (!itemStr) {
+      return null;
+  }
+  const item = JSON.parse(itemStr);
+  const now = new Date();
+  if (now.getTime() > item.expiry) {
+      localStorage.removeItem(key);
+      return null;
+  }
+  return item.value;
+};
+
