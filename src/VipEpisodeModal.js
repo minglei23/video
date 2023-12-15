6a1bbccbe97ecf895827975ad84f23a9bf5ee6d0
minglei.23 <minglei.23@hotmail.com>
2023-12-14 22:37:05 -0400

    vip

--- src/VipEpisodeModal.js
+++ src/VipEpisodeModal.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Modal, Box } from '@mui/material';
+import { Modal, Box, Button } from '@mui/material';
 
 const VipEpisodeModal = ({ open, onClose }) => {
   return (
@@ -19,10 +19,99 @@ const VipEpisodeModal = ({ open, onClose }) => {
         bgcolor: '#333',
         p: 3,
         borderRadius: '50px 50px 0 0',
+        fontFamily: 'Poppins',
+        color: '#fff',
+        display: 'flex',
+        flexDirection: 'column',
       }}>
-        <h5 style={{ fontFamily: 'Poppins', color: '#fff', textAlign: 'center' }}>
-          Next is the VIP episode.
+        <h5 style={{ margin: '3px 20px' }}>
+          Price: 1 Coins
         </h5>
+        <h5 style={{ margin: '3px 20px' }}>
+          Balance: 0 Coins
+        </h5>
+        <Button
+          style={{
+            margin: '10px 20px',
+            display: 'flex',
+            alignItems: 'stretch',
+            border: '2px solid #c70',
+            borderRadius: '10px',
+            padding: 0,
+          }}
+        >
+          <div
+            style={{
+              width: '70%',
+              display: 'flex',
+              flexDirection: 'column',
+              height: '100%',
+            }}
+          >
+            <h4 style={{ margin: '8px 0 0 0', color: '#fff', lineHeight: '1.2' }}>
+              100 Coins
+            </h4>
+            <h5 style={{ margin: '0', color: '#c70', lineHeight: '1.2' }}>
+              + 20 Coins
+            </h5>
+          </div>
+          <div
+            style={{
+              width: '30%',
+              backgroundColor: '#c70',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              borderRadius: '0 5px 5px 0',
+              height: '45px',
+            }}
+          >
+            <h3 style={{ margin: '0', color: '#fff' }}>
+              $ 1.99
+            </h3>
+          </div>
+        </Button>
+        <Button
+          style={{
+            margin: '10px 20px',
+            display: 'flex',
+            alignItems: 'stretch',
+            border: '2px solid #fa0',
+            borderRadius: '10px',
+            padding: 0,
+          }}
+        >
+          <div
+            style={{
+              width: '70%',
+              display: 'flex',
+              flexDirection: 'column',
+              height: '100%',
+            }}
+          >
+            <h4 style={{ margin: '8px 0 0 0', color: '#fff', lineHeight: '1.2' }}>
+              Become VIP
+            </h4>
+            <h5 style={{ margin: '0', color: '#fa0', lineHeight: '1.2' }}>
+              Free for all series
+            </h5>
+          </div>
+          <div
+            style={{
+              width: '30%',
+              backgroundColor: '#fa0',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              borderRadius: '0 5px 5px 0',
+              height: '45px',
+            }}
+          >
+            <h3 style={{ margin: '0', color: '#fff' }}>
+              $ 9.99
+            </h3>
+          </div>
+        </Button>
       </Box>
     </Modal>
   );
