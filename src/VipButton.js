6aa3a2082d40a4f873e4061bf6f8061d34ce59c1
minglei.23 <minglei.23@hotmail.com>
2023-12-19 23:55:06 -0400

    new pay

--- src/VipButton.js
+++ src/VipButton.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import { Button } from '@mui/material';
+import React, { useState } from 'react';
+import { Button, Modal, Box } from '@mui/material';
 import { loadStripe } from '@stripe/stripe-js';
 
 // Test public key
 const stripePromise = loadStripe("pk_test_51OFXw4Lvs8YNyX8swQOIbwVtntvw5BaZ36VFC6mIOMqk8jZdnl6DuhdiQn87b8BvP04UfqNzjI00KIwGV4scCZEk00IdJ7Htan");
 
 const VipButton = ({ border, color, content1, content2, content3 }) => {
+  const [open, setOpen] = useState(false);
+
   const handleCheckout = async () => {
     try {
       const stripe = await stripePromise;
@@ -27,49 +29,89 @@ const VipButton = ({ border, color, content1, content2, content3 }) => {
     } catch (error) {
       console.error('There was a problem with the fetch operation:', error);
     }
-  };  
+  };
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const modalStyle = {
+    fontFamily: 'Poppins',
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    bgcolor: '#222',
+    border: '1px solid #555',
+    boxShadow: 24,
+    p: 2,
+    borderRadius: 2,
+    color: '#fff',
+    width: '70%',
+    textAlign: 'center',
+  };
+
   return (
-    <Button onClick={handleCheckout}
-      style={{
-        margin: '10px 20px',
-        display: 'flex',
-        alignItems: 'stretch',
-        border: border,
-        borderRadius: '10px',
-        padding: 0,
-      }}
-    >
-      <div
+    <>
+      <Button onClick={handleOpen}
         style={{
-          width: '70%',
+          margin: '10px 20px',
           display: 'flex',
-          flexDirection: 'column',
-          height: '100%',
+          alignItems: 'stretch',
+          border: border,
+          borderRadius: '10px',
+          padding: 0,
         }}
       >
-        <h4 style={{ margin: '8px 0 0 0', color: '#fff', lineHeight: '1.2' }}>
-          {content1}
-        </h4>
-        <h5 style={{ margin: '0', color: color, lineHeight: '1.2' }}>
-          {content2}
-        </h5>
-      </div>
-      <div
-        style={{
-          width: '30%',
-          backgroundColor: color,
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          borderRadius: '0 5px 5px 0',
-          height: '45px',
-        }}
+        <div
+          style={{
+            width: '70%',
+            display: 'flex',
+            flexDirection: 'column',
+            height: '100%',
+          }}
+        >
+          <h4 style={{ margin: '8px 0 0 0', color: '#fff', lineHeight: '1.2' }}>
+            {content1}
+          </h4>
+          <h5 style={{ margin: '0', color: color, lineHeight: '1.2' }}>
+            {content2}
+          </h5>
+        </div>
+        <div
+          style={{
+            width: '30%',
+            backgroundColor: color,
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            borderRadius: '0 5px 5px 0',
+            height: '45px',
+          }}
+        >
+          <h3 style={{ margin: '0', color: '#fff' }}>
+            {content3}
+          </h3>
+        </div>
+      </Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
       >
-        <h3 style={{ margin: '0', color: '#fff' }}>
-          {content3}
-        </h3>
-      </div>
-    </Button>
+        <Box sx={modalStyle}>
+          <h5>{`${content1} ${content2}`}</h5>
+          <h5>{`${content3} pay by`}</h5>
+          <Button onClick={handleCheckout} style={{ background: '#6772e5', borderRadius: '10px', color: '#fff', margin: '5px 20px 20px 0', width: '90px' }}>Stripe</Button>
+          <Button style={{ background: '#009cde', borderRadius: '10px', color: '#fff', margin: '5px 0 20px 0', width: '90px' }}>Paypal</Button>
+        </Box>
+      </Modal>
+    </>
   );
 };
 
