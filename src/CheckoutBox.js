c76292ad1691bbe498cb25c356010bda23a34e3e
minglei.23 <minglei.23@hotmail.com>
2024-01-08 01:51:03 -0400

    test

--- src/CheckoutBox.js
+++ src/CheckoutBox.js
@@ -7,7 +7,7 @@ import { GetUser } from './cache';
 // Test public key
 const stripePromise = loadStripe("pk_live_51OFXw4Lvs8YNyX8sfVvnRljrcKEH6Mn9iDlZk9hbby4dAdNxCet4rjqIkJku2nQ6KHydOXluv1qTWyhHDPb2LJh100FDkMQqVf");
 const product = "price_1OW6LHLvs8YNyX8svyQwfJas";
-const url = "https://realshort.tv/store"
+const url = "https://realshort.tv/profile"
 
 const CheckoutBox = ({ coins, bonus, price }) => {
 
