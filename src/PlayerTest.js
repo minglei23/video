725373d3470e22e863ceac3ae9dbefb370f16ac0
minglei.23 <minglei.23@hotmail.com>
2024-01-19 02:38:00 -0400

    test

--- src/PlayerTest.js
+++ src/PlayerTest.js
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useRef } from 'react';
 
 const PlayerTest = () => {
+  const videoRef = useRef(null);
+
+  const togglePlay = () => {
+    if (videoRef.current) {
+      if (videoRef.current.paused) {
+        videoRef.current.play();
+      } else {
+        videoRef.current.pause();
+      }
+    }
+  };
+
   return (
     <div style={{ position: "fixed", height: "100%", width: "100%", backgroundColor: "#111" }}>
       <video
+        ref={videoRef}
         src="https://series-store.obs.ap-southeast-1.myhuaweicloud.com/13002/1.mp4"
         loop
         playsInline
+        onClick={togglePlay}
         style={{ width: "100%", height: "100%", objectFit: "contain" }}
         crossOrigin="anonymous"
-        controls
       >
         <track
           default
