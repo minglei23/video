d783db00436bd2ab569283d60f524448b97bb38f
minglei.23 <minglei.23@hotmail.com>
2024-02-14 18:40:00 -0400

    new

--- src/Distribution.js
+++ src/Distribution.js
@@ -55,19 +55,21 @@ export default function Distribution() {
       <Table>
         <TableHead>
           <TableRow>
-            <TableCell>User</TableCell>
+            <TableCell>User ID</TableCell>
             <TableCell>Spend</TableCell>
             <TableCell>Date</TableCell>
             <TableCell>Commission</TableCell>
+            <TableCell>Amount</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {list && list.map((item, index) => (
             <TableRow key={index}>
               <TableCell>{item.UserID}</TableCell>
-              <TableCell>${item.Spend}</TableCell>
+              <TableCell>{item.Spend.toFixed(2)}</TableCell>
               <TableCell>{item.Date}</TableCell>
               <TableCell>{item.Commission * 100}%</TableCell>
+              <TableCell>{(item.Spend * item.Commission).toFixed(2)}</TableCell>
             </TableRow>
           ))}
         </TableBody>
