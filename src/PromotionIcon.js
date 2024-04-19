77afb6295107da3ec03ba4f2dce65a9acad735fe
minglei.23 <minglei.23@hotmail.com>
2024-04-18 21:47:34 -0300

    modal

--- src/PromotionIcon.js
+++ src/PromotionIcon.js
@@ -1,41 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
 
 export default function PromotionIcon() {
   const navigate = useNavigate();
+  const [open, setOpen] = useState(true);
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   const handlePromotionClick = () => {
+    handleClose();
     navigate(`/promotion`);
   };
 
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 350,
+    height: 50,
+    bgcolor: 'transparent',
+    border: 'none',
+    boxShadow: 0,
+    p: 4
+  };
+
   return (
-    <div
-      onClick={handlePromotionClick}
-      style={{
-        position: 'relative',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        padding: '10px',
-        backgroundImage: 'linear-gradient(to right, #502, #205)',
-      }}
-    >
-      <h1 style={{ margin: '10px', fontWeight: 'bold', fontSize: '18px' }}>First Time Promotion</h1>
-      <div style={{
-        position: 'absolute',
-        top: -5,
-        right: 10,
-        backgroundColor: '#f35',
-        color: 'white',
-        padding: '3px 6px',
-        fontSize: '12px',
-        fontWeight: 'bold',
-        zIndex: 100,
-        borderRadius: '5px'
-      }}>
-        Up to 88% Off
-      </div>
+    <div>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style} onClick={e => e.stopPropagation()}>
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              justifyContent: 'center',
+              padding: '10px',
+              backgroundImage: 'linear-gradient(to right, #502, #205)',
+              borderRadius: '5px'
+            }}
+            onClick={handlePromotionClick}
+          >
+            <h1 style={{ margin: '10px', fontWeight: 'bold', fontSize: '18px', color: 'white' }}>First Time Promotion</h1>
+            <div style={{
+              position: 'absolute',
+              top: 20,
+              right: 50,
+              backgroundColor: '#f35',
+              color: 'white',
+              padding: '3px 6px',
+              fontSize: '12px',
+              fontWeight: 'bold',
+              zIndex: 100,
+              borderRadius: '5px'
+            }}>
+              Up to 88% Off
+            </div>
+          </div>
+        </Box>
+      </Modal>
     </div>
   );
 }
